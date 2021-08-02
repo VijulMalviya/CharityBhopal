6fd8aaaabd7481009e0a55aaf9af4bb0b0af8d12
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-08-02 12:34:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#bar").click(function(){
+      $(".navlinks").slideToggle("slow");
+    });
+  });
+  $(".navlinks ul li").click(function(){
+    $(".navlinks").slideUp("slow");
+  });
